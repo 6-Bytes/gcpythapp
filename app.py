4625e7891f567b20ad21c073c99dcb2c590e6792
Tanker <code006.byt@gmail.com>
2025-04-21 16:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+SocketIo.run(app, debug=True, allow_unsafe_werkzeug=True)
 
 @app.route('/')
 def index():
@@ -15,4 +15,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    SocketIo.run(app, debug=True)
